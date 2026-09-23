9771c71758988375371a6d3f53acb37ee60ae24c
Israel Damiekumo <81820172+MrPie2@users.noreply.github.com>
2026-09-23 01:28:47 -0700

    Fix production JavaScript loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/vesselhost.css') }}">
-    @vite(['resources/js/app.js'])
 </head>
 <body>
 {{ $slot ?? '' }}
 @yield('content')
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
 </body>
 </html>
